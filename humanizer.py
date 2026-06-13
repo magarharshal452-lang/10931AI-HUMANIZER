75fd4d65a040ff3ee7ff4c2a17c82425a9420bb7
magarharshal452-lang <magarharshal452@gmail.com>
2026-06-13 17:43:51 +0530

    Added Llama 3 humanizer

--- humanizer.py
+++ humanizer.py
@@ -1,1 +1,23 @@
+from transformers import pipeline
+from prompts import PROMPTS
 
+generator = pipeline(
+    "text-generation",
+    model="meta-llama/Meta-Llama-3-8B-Instruct"
+)
+
+def humanize(text, mode):
+
+    prompt = PROMPTS[mode].format(
+        text=text
+    )
+
+    result = generator(
+        prompt,
+        max_new_tokens=300,
+        do_sample=True,
+        temperature=0.7,
+        top_p=0.9
+    )
+
+    return result[0]["generated_text"]
